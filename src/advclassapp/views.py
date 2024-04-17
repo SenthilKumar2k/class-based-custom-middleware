d31172d303d84c5d78cfe4b5ac0d34dfafc60812
SenthilKumar2k <senthil2492000@gmail.com>
2024-04-17 10:55:43 +0530

    break point implementation

--- src/advclassapp/views.py
+++ src/advclassapp/views.py
@@ -1,9 +1,11 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+import pdb
 
 @csrf_exempt
 def checkindex(request, operation=None):
     print("custom request data", request.custom_data)
+    pdb.set_trace()
     if request.operation == 'update':
         raise Exception("exception raised from the views")
     return JsonResponse({"message":"request data verified succesfully"})
